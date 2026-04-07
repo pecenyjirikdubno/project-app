bf5d677629921b34405287e2af41bb36926f2dcf
pecenyjirikdubno <peceny.jirik@gmail.com>
2026-04-07 17:31:01 +0200

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
